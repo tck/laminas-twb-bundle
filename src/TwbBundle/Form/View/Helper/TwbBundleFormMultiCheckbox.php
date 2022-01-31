55f4bdfebf52ad1cbbbaa06c51588b5d6de74a0f
Alexandre Lemaire <saeven@saeven.net>
2022-01-31 11:04:18 -0500

    Adjusted to deal with an XSS reflection vector in Laminas Form

--- src/TwbBundle/Form/View/Helper/TwbBundleFormMultiCheckbox.php
+++ src/TwbBundle/Form/View/Helper/TwbBundleFormMultiCheckbox.php
@@ -8,24 +8,25 @@ use Laminas\Form\ElementInterface;
 class TwbBundleFormMultiCheckbox extends FormMultiCheckbox
 {
     /**
-     * @see FormMultiCheckbox::render()
      * @param ElementInterface $oElement
+     *
      * @return string
+     * @see FormMultiCheckbox::render()
      */
-    public function render(ElementInterface $oElement)
+    public function render(ElementInterface $oElement): string
     {
         $aElementOptions = $oElement->getOptions();
 
         // For inline multi-checkbox
         if (isset($aElementOptions['inline']) && $aElementOptions['inline'] == true) {
             $this->setSeparator('');
-            $oElement->setLabelAttributes(array('class' => 'checkbox-inline'));
+            $oElement->setLabelAttributes(['class' => 'checkbox-inline']);
 
             return sprintf('%s', parent::render($oElement));
         }
 
         $this->setSeparator('</div><div class="checkbox">');
-        $oElement->setLabelAttributes(array('class' => 'checkbox'));
+        $oElement->setLabelAttributes(['class' => 'checkbox']);
 
         return sprintf('<div class="checkbox">%s</div>', parent::render($oElement));
     }
